4b8378dfc8870bc4a1c7bcfc2d565548479e0c63
Janardhana <janachet@amazon.com>
2021-10-24 19:35:41 +0530

    refactored static pages serving using templates and layouts

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejs = require('ejs')
+
+app.set('view engine','ejs')
 
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log("App listening on port 3000")
 })
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.render('index');
 })
 
 app.get('/about',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    res.render('about');
 })
 
 app.get('/contact',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    res.render('contact');
 })
 
 app.get('/post',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    res.render('post');
 })
     
