653a9c6d7cf09ac96f0ca8e8c0b8b8772c3f286c
Janardhana <janachet@amazon.com>
2021-10-24 18:36:08 +0530

    all basic pages added, css templates added

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
 
 app.listen(3000,()=>{
     console.log("App listening on port 3000")
 })
 
 app.get('/',(req,res)=>{
-    res.json({
-        name: 'Greg Lim'
-    })
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+})
+
+app.get('/about',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+})
+
+app.get('/contact',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+})
+
+app.get('/post',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
 })
     
