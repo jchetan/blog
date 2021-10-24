695512b6e980af20b9af09f85d3361001d5c16ae
Janardhana <janachet@amazon.com>
2021-10-24 18:40:17 +0530

    made PORT as a variable to enable running on heroku

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 app.use(express.static('public'));
 
-app.listen(3000,()=>{
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
     console.log("App listening on port 3000")
 })
 
